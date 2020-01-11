075fa45846e9677cf53ae662318d8616fb9e4700
rachsg7 <sch19051@byui.edu>
2020-01-10 20:45:52 -0800

    Final

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 let lastModified = document.lastModified;
+let date = new Date();
 
 document.getElementById("timeUpdated").innerHTML = lastModified;
+document.getElementById("currentYear").innerHTML = date.getFullYear();
